7ab160147d3aafb70d58c6b895ce189719fbe050
Al Sweigart <asweigart@gmail.com>
2018-10-15 17:21:39 -0700

    Simplifying the way we run the tournaments. Renaming the examples file to be lowercase.

--- config.py
+++
@@ -1,7 +0,0 @@
-games = 100
-ui = 'web'
-bots = [
-    ['zombieBotExamples.py', 'RandomCoinFlipZombie', 'Random Bot'],
-    ['zombieBotExamples.py', 'MonteCarloZombie', 'Monte Carlo Bot', 40, 20],
-    ['zombieBotExamples.py', 'MinNumShotgunsThenStopsZombie', 'Min Shotguns Bot', 2],
-]
